950e674dedde53164a327304033a067528483ff8
Marcosplira <61232814+Marcosplira@users.noreply.github.com>
2024-07-25 15:00:04 +0000

    fazendo alterações

---
+++ campo/loja/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views 
+
+
+urlpatterns = [
+    path('pesquisar',views.pesquisar, name='pesquisar'),
+
+]
--- campo/loja/views.py
+++ campo/loja/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Produto 
+from django.db.models import Q  
 
-# Create your views here.
+def pesquisar(request):
+    resultado = Produto.objects.filter(nome="Laranja")
+    # print(resultado)
+    # print("Opa Chegou aqui")
+    return HttpResponse(resultado) 
